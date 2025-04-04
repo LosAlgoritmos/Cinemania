39b254ab5ac575b371ed1643163945005d974f18
Alper Kale <mr.alperkale@gmail.com>
2025-04-04 06:00:29 +0300

    alper commit 1.0.4 movies.js  test ön yükleme

--- src/js/movies.js
+++ src/js/movies.js
@@ -1,8 +1,25 @@
+import { showLoader, hideLoader, fetchWithLoader } from './loader.js';
+
 const options = {method: 'GET', headers: {accept: 'application/json'}};
 
-fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+// fetchWithLoader fonksiyonunu kullanarak veri çekiyoruz
+async function getMovies() {
+  try {
+    const data = await fetchWithLoader(
+      'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', 
+      options
+    );
+    console.log(data);
+    // Burada verilerin işlenmesi ve ekrana yansıtılması
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Sayfanın yüklenmesi durumunda çalıştırılacak
+document.addEventListener('DOMContentLoaded', () => {
+  getMovies();
+});
 
 
