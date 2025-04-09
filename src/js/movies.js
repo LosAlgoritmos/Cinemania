8b2ba73ee5653c1720fbe11ed289d16fa38b2e73
Kaan <103496597+RoyalNegative@users.noreply.github.com>
2025-04-10 01:42:03 +0300

    change

--- src/js/movies.js
+++ src/js/movies.js
@@ -235,3 +235,17 @@ document.addEventListener('DOMContentLoaded', () => {
   
   getMovies();
 });
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGFlMTQ1ZTQyYzc4MDQ5YjI3Y2MwY2NhZWU1NGE3NSIsIm5iZiI6MTc0MzYzNzA0MC4yMzksInN1YiI6IjY3ZWRjYTMwMDM1NDBjZjhlNTYyODgwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tuTxlFoUnsTSw7K5siU94XHqec4Jgt-IVZeRxvzIv2Y'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
