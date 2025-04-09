90111db2ea758dae4b3d735de18e9a45db148608
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-09 22:25:16 +0300

    Add catalog.js with initial imports and DOMContentLoaded event handling

---
+++ src/catalog.js
@@ -0,0 +1,29 @@
+// @murselsen
+import './js/navbar.js';
+import './js/hero.js';
+// @AlperKale
+import { showLoader, hideLoader } from './js/loader.js';
+import './js/scroll-up.js';
+// @RoyalNegative
+import './js/movies.js';
+// @sumeyyeterzi
+import './js/infoPopup.js';
+// @erdem6161
+import './js/footer.js';
+import './js/members-modal.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Sayfanın tamamen yüklenmesini bekleyelim
+  // Bu sırada showLoader'ı çağırarak loader'ı gösteriyoruz
+  showLoader();
+  
+  // Sayfa tamamen yüklendiğinde loader'ı gizle
+  window.addEventListener('load', () => {
+    // Loader'ı gizle
+    // hideLoader fonksiyonu zaten minimum gösterim süresini dikkate alacak
+    setTimeout(() => {
+      hideLoader();
+    }, 500); // Sayfanın tam yüklenmesinden sonra 0.5 saniye daha bekle
+  });
+});
